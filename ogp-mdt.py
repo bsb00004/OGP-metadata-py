2e2ebf9d74d62ab0cee76ad4005302df673630a7
Kevin R Dyke <krdyke@gmail.com>
2014-08-14 11:02:35 -0500

    add mgs option

--- ogp-mdt.py
+++ ogp-mdt.py
@@ -22,22 +22,34 @@ except ImportError:
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("workspace",help="indicate the path where the metadata to be converted is contained")
-    parser.add_argument("output_path",help="indicate the path where the output should be sent")
-    parser.add_argument("metadata_type",help="Metadata standard used for input XMLs. Acceptable values are FGDC or MGMG")
+    parser.add_argument("-m","--mgs", action="store_true", help="shortcut for MGS project")
+    parser.add_argument("workspace", nargs='?', help="indicate the path where the metadata to be converted is contained")
+    parser.add_argument("output_path", nargs='?', help="indicate the path where the output should be sent")
+    parser.add_argument("metadata_type", nargs='?', help="Metadata standard used for input XMLs. Acceptable values are FGDC or MGMG")
     args = parser.parse_args()
-  
-    # set workspace, check if it's an absolute or relative path and make changes as needed 
-    ws = args.workspace
-    if not os.path.isabs(ws):
-        ws = os.path.abspath(os.path.relpath(ws,os.getcwd()))
 
-    # same for output path
-    output = args.output_path
-    if not os.path.isabs(output):
-        output = os.path.abspath(os.path.relpath(output,os.getcwd()))
-    
+    if args.mgs:
+        mgs_record = raw_input("Please enter record number: ")
+        ws = os.path.join("D:\drive\Map Library Projects\MGS\Records",
+                          mgs_record,
+                          "final_XMLs")
+        output = os.path.join(ws,"OGP")
+
+
+    else:
+
+        # set workspace, check if it's an absolute or relative path and make changes as needed
+        ws = args.workspace
+        if not os.path.isabs(ws):
+            ws = os.path.abspath(os.path.relpath(ws,os.getcwd()))
+
+        # same for output path
+        output = args.output_path
+        if not os.path.isabs(output):
+            output = os.path.abspath(os.path.relpath(output,os.getcwd()))
+
     md = args.metadata_type.lower()
+
     if md not in METADATA_OPTIONS:
         sys.exit('Invalid metadata standard. Supported options are "' + '", "'.join(METADATA_OPTIONS) + '". Please try again.')
     
