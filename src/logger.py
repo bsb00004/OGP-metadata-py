ad955d5ec8fb5ccd00455907bb3a3c2bc3903ac6
Kevin R Dyke <krdyke@gmail.com>
2014-11-25 09:59:25 -0600

    adds json support for logging

--- src/logger.py
+++ src/logger.py
@@ -17,10 +17,10 @@ class Logger(object):
         self.output_location = output_location
         self._create_log()
 
-    def _create_log():
-        if self.output_format = "csv":
+    def _create_log(self):
+        if self.output_format == "csv":
             self._create_csv()
-        elif self.output_format = "json":
+        elif self.output_format == "json":
             self._create_json()
             
 
@@ -33,17 +33,38 @@ class Logger(object):
         self.log = {}
 
     def write(self, item, message):
-        if self.output_format = "csv":
-            self._write_csv(item,message)
-        elif self.output_format = "json":
-            self._create_json()
-
+        if self.output_format == "csv":
+            self._write_csv(item, message)
+        elif self.output_format == "json":
+            self._write_json(item, message)
 
     def _write_csv(self, item, message):
         s = os.path.split(item)
         self.log.writerow([s[0], s[1], message])
 
     def _write_json(self, item, message):
+        if item in self.log:
+            self.log[item].append(message)
+        else:
+            self.log[item] = [message]
+        print self.log
+
+    def _close_csv(self):
+        self.csvfile.close()
+
+    def _close_json(self):
+        """
+        Close out JSON log. Currently writes to a file just like CSV
+        TODO: add option to return JSON rather than write to file
+        """
+        self.filename = '__ogp-mdt-log-' + str(time.time()).replace('.', '') + '.json'
+        output = open(os.path.join(self.output_location, self.filename), mode='a')
+        output.write(json.dumps(self.log))
 
     def close(self):
-        self.csvfile.close()
+        if self.output_format == "csv":
+            self._close_csv()
+        elif self.output_format == "json":
+            self._close_json()
+
+        
