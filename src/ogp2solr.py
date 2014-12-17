3a3eeed0d82592c6cfc80d0d8da003b99b68acff
krd <krdyke@gmail.com>
2014-12-17 16:52:18 -0500

    merge

--- src/ogp2solr.py
+++ src/ogp2solr.py
@@ -2,39 +2,36 @@ from lib import pysolr
 
 class SolrOGP(object):
 
-    def __init__(self, url=None):
+    def __init__(self, url="http://ec2-54-87-229-228.compute-1.amazonaws.com:8080/solr/collection1/"):
 
         # defaults to UMN OGP
-        if url is None:
-            url = "http://ec2-54-87-229-228.compute-1.amazonaws.com:8080/solr/collection1/"
-
         self.solr_url = url
         self.solr = self._connect_to_solr()
 
-    def escape_query(self,raw_query):
+    def escape_query(self, raw_query):
         """
         Escape single quotes in value. May or may not be worth a damn at the moment.
         """
         return raw_query.replace("'","\'")
 
-    def run_query(self,query):
+    def run_query(self, query, **kwargs):
         """
         Query the connected Solr index
         """
         q = self.escape_query(query)
-        return self.solr.search(q)
+        return self.solr.search(q, **kwargs)
 
     def add_dict_to_solr(self,record_dict):
         self.solr.add([record_dict])
 
-    def add_to_solr(self,tree):
+    def add_to_solr(self, tree):
         """
         Adds a document to Solr. Accepts an ElementTree as argument.
         """
         d = self.tree_to_dict(tree)
         self.solr.add([d])
 
-    def add_to_solr_bulk(self,list_of_trees):
+    def add_to_solr_bulk(self, list_of_trees):
         """
         Adds a bunch of records. Accepts a list of ElementTrees as argument
         """
@@ -42,13 +39,13 @@ class SolrOGP(object):
         print "Adding/updating %s documents in Solr index" % (len(list_of_trees))   
         self.solr.add(list_of_dicts)
 
-    def _build_delete_query(self,list_of_layer_ids):
+    def _build_delete_query(self, list_of_layer_ids):
         return "LayerId:(" + " ".join(list_of_layer_ids) + ")"
 
-    def delete_query(self,query):
+    def delete_query(self, query):
         self.solr.delete(q=self.escape_query(query))
 
-    def delete_from_solr(self,list_of_layer_ids):
+    def delete_from_solr(self, list_of_layer_ids):
         """
         Delete a list of LayerId values from Solr.
         """
