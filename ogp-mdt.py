c45094cb5b5725114fa7c1a7b139e16d9a255f8f
Kevin Dyke <krdyke@gmail.com>
2015-11-20 10:32:45 -0600

    gdrs option back in

--- ogp-mdt.py
+++ ogp-mdt.py
@@ -8,14 +8,14 @@ import fnmatch
 import pdb
 from src import md2ogp
 
-METADATA_OPTIONS = ['mgmg','fgdc','arcgis','marc','iso','guess','eod']
+METADATA_OPTIONS = ['mgmg','fgdc','arcgis','marc','iso','guess','eod','gdrs']
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("input_path",help="indicate the path where the metadata to be converted is contained")
     parser.add_argument("output_path",help="indicate the path where the output should be sent, or 'same' if you want to output to the same path as the input")
-    parser.add_argument("metadata_type",help="Metadata standard used for input XMLs. Acceptable values are FGDC, MGMG, ISO, EOD, MARC, or GUESS (which takes a guess)")
+    parser.add_argument("metadata_type",help="Metadata standard used for input XMLs. Acceptable values are FGDC, MGMG, ISO, EOD, MARC, GDRS or GUESS (which takes a guess)")
     parser.add_argument("-i","--indirect",action="store_true",help="If the links in the metadata do not directly return a binary file (e.g. a zip archive), set this option")
     parser.add_argument("-l","--log_only",action="store_true",help="If you just want the log written (i.e. no output XMLs). Useful for metadata cleanup.")
     parser.add_argument("-z","--zip", action="store_true", help="Output a zip file with the metadata files in it")
@@ -31,12 +31,10 @@ def main():
 
     output = args.output_path
 
-
     if output.lower() != "same" and not os.path.isabs(output):
         output = os.path.abspath(os.path.relpath(output,os.getcwd()))
     elif output.lower() == "same":
         output = ws
-
 
     md = args.metadata_type.lower()
 
